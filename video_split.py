58b663ee5ca9e19861dbdae9c156ac5140b9bc3e
Kajaqq <kajos200@gmail.com>
2025-06-19 00:52:47 +0200

    Remove redundant code and imports.

--- video_split.py
+++ video_split.py
@@ -11,14 +11,13 @@ extension_map = {
     'pcm_s16le': 'wav'
 }
 
-
+# noinspection PyInconsistentReturns
 def detect_video(file_path):
     with open(file_path, 'r') as container:
         if container.streams.video:
             return True
         else:
             return False
-    return None
 
 def get_audio_codec(video_path):
     with open(video_path, 'r') as container:
@@ -57,22 +56,9 @@ def extract_audio(video_path, audio_path):
     except FileNotFoundError:
         print(f"Error: File '{video_path}' not found.")
         return False
-    except Exception as e:
-        print(f"An error occurred during extraction: {e}")
-        return False
     except IndexError:
         print(f"Error: No audio stream found in '{video_path}' for extraction.")
         return False
-
-if __name__ == "__main__":
-    input_file = 'video_file.mp4'
-    if not detect_video(input_file):
-        print("The input file is not a video.")
-        exit(1)
-    input_video = input_file
-    output_audio = get_output_path(input_video)
-    print(f"Extracting audio from '{input_video}'...")
-    extract_audio(input_video, output_audio)
-    print(
-        f"Audio extracted successfully to '{output_audio}'."
-    )
+    except Exception as e:
+        print(f"An error occurred during extraction: {e}")
+        return False
